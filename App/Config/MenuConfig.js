5c1eaffdf8725f91215d98e16a763e6a8902051f
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-05-24 17:34:09 -0700

    [feat] Change endpoint hit to be EC2 instance instead of GCP

--- App/Config/MenuConfig.js
+++ App/Config/MenuConfig.js
@@ -2,5 +2,5 @@ import DeviceInfo from 'react-native-device-info'
 
 export default {
   // domain: DeviceInfo.getManufacturer() === 'Genymotion' ? 'http://10.0.3.2:1337' : 'http://10.0.2.2:1337'
-  domain: 'http://barambe-2.appspot.com'
+  domain: 'http://ec2-54-197-9-47.compute-1.amazonaws.com'
 }
